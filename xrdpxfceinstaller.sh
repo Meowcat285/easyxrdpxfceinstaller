8bebeeac1ea5ca7b112496947d15fe0d996f88b1
Meowcat285 <Meowcat285@users.noreply.github.com>
2019-03-14 18:15:42 -0700

    Update xrdpxfceinstaller.sh

--- xrdpxfceinstaller.sh
+++ xrdpxfceinstaller.sh
@@ -1,2 +1,10 @@
 echo "Please make sure you are running this script as root or else this will fail"
 apt-get update
+apt-get install xrdp
+echo "XRDP installed, installing XFCE 4 base with some extra goodies"
+apt-get install xfce4
+apt-get install xfce4-terminal
+apt-get install gnome-icon-theme-full tango-icon-theme
+echo "Done installing XFCE 4, preparing services"
+echo xfce4-session >~/.xsession
+echo startxfce4 >/etc/xrdp/startwm.sh
